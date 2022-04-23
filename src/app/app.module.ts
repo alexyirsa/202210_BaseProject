58903ed87d022a25786dfb0b863bfa2e5f5797bc
AlexanderYirsa <v.yirsa@uniandes.edu.co>
2022-04-22 21:34:37 -0500

    Cambios en tutorial No.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookModule } from './book/book.module';
+import { EditorialModule } from './editorial/editorial.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BookModule,
+    EditorialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
