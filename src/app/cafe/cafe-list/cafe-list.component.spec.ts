98b417c1b16e83df6dd05ed7be621a5fa4da1b61
AlexanderYirsa <v.yirsa@uniandes.edu.co>
2022-04-30 23:31:55 -0500

    Desarrollo de pruebas para cafés

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
+
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CafeListComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -19,10 +27,30 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*it('should have an img element ', () => {
+    expect(debug.query(By.css('img')).attributes['alt']).toEqual(
+      component.cafes[0].nombre
+    );
+  });*/
+
 });
